6c7898435bcc12c5c1531319e7b555775508b8ea
Felipe Romo <felipe@romofel.dev>
2021-05-20 16:08:30 -0500

    Add constants
    Adds constant values based on the style guide provided

---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/painting.dart';
+
+// Layout
+const kMobileWidth = 375;
+const kDesktopWidth = 1440;
+
+// Colors - Primary
+const kMainBackgroundColor = HSLColor.fromAHSL(1, 233, .47, .07);
+const kCardBackgroundColor = HSLColor.fromAHSL(1, 244, .38, .16);
+const kAccentColor = HSLColor.fromAHSL(1, 277, .64, .61);
+
+// Colors - Neutral
+const kMainHeadingColor = HSLColor.fromAHSL(1, 0, 0, 1);
+const kStatsColor = HSLColor.fromAHSL(1, 0, 0, 1);
+const kMainParagraphColor = HSLColor.fromAHSL(.75, 0, 0, 1);
+const kStatHeadingsColor = HSLColor.fromAHSL(.6, 0, 0, 1);
+
+// Typography
+const kBodyCopyFontSize = 15;
+const kInterFontWeights = [400, 700];
+const kLexendDecaFontWeights = [400];
