798dbcee525a84e4197ec47edf62fbda04d4ae26
Felipe Romo <felipe@romofel.dev>
2021-05-20 14:00:25 -0500

    Scaffold the layout skeleton for mobile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text('Home Page'),
+        child: Column(
+          children: <Widget>[
+            FlutterLogo(),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  // this probably will be a textspan / richtext
+                  Text('Get insights that help your business grow'),
+                  Text(
+                      'Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.'),
+                  Column(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Text('10k+'),
+                          Text('COMPANIES'),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text('314'),
+                          Text('TEMPLATES'),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text('12M+'),
+                          Text('QUERIES'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
