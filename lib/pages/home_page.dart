9c9e92ac725474455165cc1941930f346a6043de
Felipe Romo <felipe@romofel.dev>
2021-05-20 16:42:29 -0500

    Clip the edges of the card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,47 +8,50 @@ class HomePage extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
         color: kMainBackgroundColor.toColor(),
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              'assets/image-header-mobile.jpg',
-              width: 290,
-              color: kAccentColor.toColor(),
-              colorBlendMode: BlendMode.softLight,
-            ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  // this probably will be a textspan / richtext
-                  Text('Get insights that help your business grow'),
-                  Text(
-                      'Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.'),
-                  Column(
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Text('10k+'),
-                          Text('COMPANIES'),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text('314'),
-                          Text('TEMPLATES'),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text('12M+'),
-                          Text('QUERIES'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'assets/image-header-mobile.jpg',
+                width: 290,
+                color: kAccentColor.toColor(),
+                colorBlendMode: BlendMode.softLight,
               ),
-            ),
-          ],
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    // this probably will be a textspan / richtext
+                    Text('Get insights that help your business grow'),
+                    Text(
+                        'Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.'),
+                    Column(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text('10k+'),
+                            Text('COMPANIES'),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('314'),
+                            Text('TEMPLATES'),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('12M+'),
+                            Text('QUERIES'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
